a5a2bd5d342aa23ae9b96fb0c71f547fc19c6a98
lurttana <102549399+lurttana@users.noreply.github.com>
2025-04-27 20:04:08 +0300

    mariadb

--- FlightGame-Mico2/connectDB.py
+++ FlightGame-Mico2/connectDB.py
@@ -1,18 +1,17 @@
 from os import getenv
-import mysql.connector
+import mariadb
 from dotenv import load_dotenv
 from enemy import CreateEnemy
 from checkpoint import Checkpoint
 
 load_dotenv()
 
-yhteys = mysql.connector.connect(
+yhteys = mariadb.connect(
          host=getenv('DB_HOST'),
          user=getenv('DB_USER'),
-         port= getenv('DB_PORT'),
+         port= int(getenv('DB_PORT')),
          database=getenv('DB_DATABASE'),
          password=getenv('DB_PASSWORD'),
-         collation='utf8mb4_general_ci',
          autocommit=True
          )
 
